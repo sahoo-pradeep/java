23bfa9c8f7f27d7aff8b416d34b4b19077cca5bb
Pradeep Kumar Sahoo <pradeep17sep@gmail.com>
2020-05-20 12:27:31 +0530

    changes in swagger config

--- src/main/java/projects/sahoo/myspringboot/config/SwaggerConfig.java
+++ src/main/java/projects/sahoo/myspringboot/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package projects.sahoo.myspringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -21,7 +22,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiEndPointsInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("projects.sahoo.myspringboot.controllers"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
